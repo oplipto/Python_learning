ed8c8e5b0a54f83da5441233725c4691e9f21b6c
Rohit Pandey <croods660@gmail.com>
2024-03-27 11:23:27 +0530

    My Python Learning Files

--- 11_Projects/12_discord_bot.py
+++
@@ -1,6 +0,0 @@
-
-
-
-Discord_Server_bot_key = 'MTIxNzczMDEwNjk0MDcxOTExMA.GNqzh5.pctDIvwtS4TaJy19GBEOi2lpp7FPlqGDwfB0DI'
-
-my_discord_bot = 'https://discord.com/oauth2/authorize?client_id=1217730106940719110&permissions=2048&scope=bot'
---
+++ 11_Projects/14_terminal_game.py
@@ -0,0 +1,13 @@
+
+
+answer = input("Are we there yet?")
+
+while answer != "yes":
+    answer = input("Are we there yet?")
+
+
+
+print("""Are we there yet? One more hour
+Are we there yet? Almost there
+Are we there yet? Don't make me pull over
+Are we there yet? Yes""")
